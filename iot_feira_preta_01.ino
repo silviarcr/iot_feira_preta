1978fb64af9193c2c5aee261d08d47cb595670a7
SILVIA REGINA COELHO DA ROSA <silvia.rcr@gmail.com>
2018-03-09 21:21:23 -0300

    Add suporte HTTP e Ethernet

--- iot_feira_preta_01.ino
+++ iot_feira_preta_01.ino
@@ -1,7 +1,3 @@
-
-//
-//https://www.mocky.io/
-//http://www.mocky.io/v2/5aa0889e3200006511e9fcdd
 #include <UIPEthernet.h>
 #include <RestClient.h>
 const int luz_verde = 2;
@@ -9,12 +5,11 @@ const int luz_amarela = 4;
 const int luz_vermelha = 7;
 int intervalo_atualizacao = 1000;
 
-//Ethernet
+// Enter a MAC address for your controller below.
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xF1, 0x77};
-EthernetClient ethClient;
 
-RestClient client("http://www.mocky.io/v2/5aa0889e3200006511e9fcdd", ethClient);
-
+EthernetClient client;
+RestClient restClient("192.168.2.36", 3000, client);
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,7 +18,7 @@ void setup() {
   pinMode(luz_vermelha, OUTPUT);
   Serial.begin(9600);
 
-  if(Ethernet.begin(mac)) {
+  if (Ethernet.begin(mac)) {
     Serial.println(Ethernet.localIP());
   } else {
     Serial.println(F("Falha na inicialização Ethernet"));
@@ -31,37 +26,47 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  while (Serial.available()) {
-
-    char voto = Serial.read();
-
-    switch (voto) {
-      case '1':
-        digitalWrite(luz_verde, HIGH);
-        digitalWrite(luz_amarela, LOW);
-        digitalWrite(luz_vermelha, LOW);
-        Serial.println("verde");
-        break;
-      case '2':
-        digitalWrite(luz_verde, LOW);
-        digitalWrite(luz_amarela, HIGH);
-        digitalWrite(luz_vermelha, LOW);
-        Serial.println("amarela");
-        break;
-      case '3':
-        digitalWrite(luz_verde, LOW);
-        digitalWrite(luz_amarela, LOW);
-        digitalWrite(luz_vermelha, HIGH);
-        Serial.println("vermelha");
-        break;
-      default:
-        digitalWrite(luz_verde, LOW);
-        digitalWrite(luz_amarela, LOW);
-        digitalWrite(luz_vermelha, LOW);
-    }
-
+  char resposta[16] = {};
+  int statusCode = restClient.get("/", resposta, 16);
+  if (statusCode == 200) {
+    Serial.println(F("Recebi resposta"));
+    char voto = resposta[0];
+    Serial.println(resposta);
+    atualizarLampadas(voto);
+  } else {
+    Serial.println(F("Falhou!"));
+    Serial.println(statusCode);
   }
+
+  resposta[0] = 0;
+
   delay(intervalo_atualizacao);
 }
+
+void atualizarLampadas(int voto) {
+  switch (voto) {
+    case '1':
+      digitalWrite(luz_verde, HIGH);
+      digitalWrite(luz_amarela, LOW);
+      digitalWrite(luz_vermelha, LOW);
+      Serial.println("verde");
+      break;
+    case '2':
+      digitalWrite(luz_verde, LOW);
+      digitalWrite(luz_amarela, HIGH);
+      digitalWrite(luz_vermelha, LOW);
+      Serial.println("amarela");
+      break;
+    case '3':
+      digitalWrite(luz_verde, LOW);
+      digitalWrite(luz_amarela, LOW);
+      digitalWrite(luz_vermelha, HIGH);
+      Serial.println("vermelha");
+      break;
+    default:
+      digitalWrite(luz_verde, LOW);
+      digitalWrite(luz_amarela, LOW);
+      digitalWrite(luz_vermelha, LOW);
+  }
+}
+
